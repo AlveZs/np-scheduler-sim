fa88234c9a965ef75acb0ca0e48a0b6b95027d0d
João Alves <alvezsjv@gmail.com>
2021-10-29 12:46:11 -0300

    feat(main): adiciona leitura de mais de um arquivo e catch para não atribuição

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from offlineProcedure import OfflineProcedure
 from fileReader import loadDataFromFile
+import os
 
-processorsNumber, tasks = loadDataFromFile("tasks.txt")
-OfflineProcedure(tasks, processorsNumber)
+directory = 'tasksets'
+
+for filename in os.scandir(directory):
+    if filename.is_file():
+        print(filename.path)
+        processorsNumber, tasks = loadDataFromFile(filename.path)
+        try:
+            OfflineProcedure(tasks, processorsNumber)
+            print("\n\n")
+        except ValueError as err:
+            message, assigments = err.args
+            print(message)
+            print(assigments)
