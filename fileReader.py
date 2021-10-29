32181e6619f025190036875c419da4310e10223d
João Alves <alvezsjv@gmail.com>
2021-10-29 12:47:34 -0300

    feat(fileReader): adiciona display para informações dos conjuntos de tarefas

--- fileReader.py
+++ fileReader.py
@@ -1,9 +1,27 @@
 from task import Task
 
+def tasksetInfo(processorsNumber, tasks):
+    totalUtilization = 0.0
+    print("=========================================")
+    print("Numero de processadores: ", processorsNumber)
+    for task in tasks:
+        taskUtilization = task.executionTime/task.deadline
+        print("{} c: {} d: {} U: {}".format(
+            task.name,
+            task.executionTime,
+            task.deadline,
+            round(taskUtilization, 2)
+        ))
+        totalUtilization = totalUtilization + taskUtilization
+    print("=========================================\n")
+    print("Utilizacao total do sistema: ", round(totalUtilization / processorsNumber, 2))
+
+
 def loadDataFromFile(filename):
+    Task.number = 1
     tasks = []
     f = open(filename, "r")
-    processorsNumber = int(f.readline().split()[1])           
+    processorsNumber = int(f.readline().split()[1])
     for line in f:
         l=[float(j) for j in line.split()]
         # period = float(l[-2])
@@ -12,4 +30,5 @@ def loadDataFromFile(filename):
         newTask = Task(execution, deadline)
         tasks.append(newTask)
     f.close()
+    tasksetInfo(processorsNumber, tasks)
     return processorsNumber, tasks
